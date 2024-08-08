b51538fc5cbbdfb1fc57f1da25b68a1681856f96
cfs <2733828240@qq.com>
2024-08-08 17:57:29 +0800

    第一次提交

---
+++ src/test/java/com/menghuan/MenghuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menghuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenghuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
