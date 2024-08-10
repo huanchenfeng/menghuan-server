a12f9e11e5d95e4e3060bdcaa31d1f9fef0864ad
cfs <2733828240@qq.com>
2024-08-10 09:10:19 +0800

    第二次提交

---
+++ meng-admin/src/main/java/com/meng/MengHaunApplication.java
@@ -0,0 +1,18 @@
+package com.meng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+    /**
+     * 启动程序
+     *
+     * @author ruoyi
+     */
+    @SpringBootApplication()
+    public class MengHaunApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(MengHaunApplication.class, args);
+            System.out.println("(♥◠‿◠)ﾉﾞ  梦幻启动成功   ლ(´ڡ`ლ)ﾞ\n");
+        }
+    }
